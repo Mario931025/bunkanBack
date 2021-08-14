53d6b3a8acbec1c913a699271d45099eb14beabf
Mario <mario.fes.programacion99.com>
2021-08-13 23:08:14 -0500

    estan las capretas y archivos del proyecto listos

---
+++ src/pantallas/cuenta/ConfirmarNumero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function ConfirmarNumero() {
+    return (
+        <View>
+            <Text>MARIO GARCIA</Text>
+            <Text>27 AÑOS</Text>
+            <Text>MÉXICO</Text>
+        </View>
+    )
+}
